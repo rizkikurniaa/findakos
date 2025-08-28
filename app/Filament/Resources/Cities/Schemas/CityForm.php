3ea80a66a8d4709bd6968be9097539b7a6678b69
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-08-28 23:57:20 +0700

    add City resource with form, table, and pages for managing cities

---
+++ app/Filament/Resources/Cities/Schemas/CityForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Cities\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CityForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                FileUpload::make('image')
+                    ->image()
+                    ->required()
+                    ->columnSpan(2),
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+            ]);
+    }
+}
