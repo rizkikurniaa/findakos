668604d3c964e3b4a9f77c468efd44fbf1651b91
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-08-29 19:22:06 +0700

    add view and delete actions to CitiesTable

--- app/Filament/Resources/Cities/Tables/CitiesTable.php
+++ app/Filament/Resources/Cities/Tables/CitiesTable.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Cities\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -33,7 +35,9 @@ class CitiesTable
                 //
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
