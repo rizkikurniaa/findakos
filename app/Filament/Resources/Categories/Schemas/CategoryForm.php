345153d41723f20def64301db492c012a6eabd25
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-08-29 19:22:46 +0700

    created category

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                FileUpload::make('image')
+                    ->image()
+                    ->directory('categories')
+                    ->required()
+                    ->columnSpan(2),
+                TextInput::make('name')
+                    ->required()
+                    ->live(debounce: 500)
+                    ->afterStateUpdated(function ($state, callable $set) {
+                        $set('slug', Str::slug($state));
+                    }),
+                TextInput::make('slug')
+                    ->required(),
+            ]);
+    }
+}
