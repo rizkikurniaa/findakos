a8ec921a91b75f623b903eca1db00c6b68e47c46
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-09-02 15:01:09 +0700

    register

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Interfaces\CityRepositoryInterface;
+use App\Repositories\CityRepository;
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Repositories\CategoryRepository;
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Repositories\BoardingHouseRepository;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(
+            CityRepositoryInterface::class,
+            CityRepository::class
+        );
+
+        $this->app->bind(
+            CategoryRepositoryInterface::class,
+            CategoryRepository::class
+        );
+
+        $this->app->bind(
+            BoardingHouseRepositoryInterface::class,
+            BoardingHouseRepository::class
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
