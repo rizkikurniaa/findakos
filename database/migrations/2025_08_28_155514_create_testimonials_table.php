af94c0b53687515af23cbd1f0ad8152354b943a5
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-09-02 10:30:20 +0700

    Add soft delete

--- database/migrations/2025_08_28_155514_create_testimonials_table.php
+++ database/migrations/2025_08_28_155514_create_testimonials_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('photo');
             $table->string('content');
             $table->integer('rating');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
