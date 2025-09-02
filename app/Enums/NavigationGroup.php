82b6eda4060916eebe94032bcaa05b162b8b6c6e
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-09-02 11:33:57 +0700

    create enum

---
+++ app/Enums/NavigationGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum NavigationGroup: string
+{
+    case BoardingHouse = 'Boarding House Management';
+    // case User = 'User Management';
+    // case Settings = 'Settings';
+}
