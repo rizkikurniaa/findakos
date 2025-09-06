8d403776dd77cdba5d61a129dcc0a186edd27af1
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-09-06 19:35:30 +0700

    add find

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
